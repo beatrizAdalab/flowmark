7ae875bca3180343cfdec1e58869e46ce9ca7081
beatrizGJ <beatrizgarcia.jim@gmail.com>
2020-02-26 20:31:30 +0100

    created App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Header from './components/Header'
+import LoginContext from './context/LoginContext'
+import ListClassifieds from './components/ListClassifieds'
+import DetailClassifieds from './components/DetailClassified'
+import EditClassifieds from './components/EditClassified'
+import NewClassified from './components/NewClassified'
 
-function App() {
-  return (
-    <div className="App">
-      hello world
 
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <Router>
+        <LoginContext>
+          <div className='App bg-light'>
+            <Header />
+            <main className='container'>
+              <Switch>
+                <Route path='/login' component={Login} />
+                <Route path='/register' component={Register} />
+                <Route path='/listClassifieds/' component={ListClassifieds} />
+                <Route path='/detailClassifieds/:id' component={DetailClassifieds} />
+                <Route path='/editClassifieds/:id' component={EditClassifieds} />
+                <Route path='/newClassified' component={NewClassified} />
+                <Redirect to='/login' />
+              </Switch>
+            </main>
+          </div>
+        </LoginContext>
+      </Router>
+    );
+  }
 }
 
 export default App;
